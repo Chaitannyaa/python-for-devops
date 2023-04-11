fd9759f0fbca1a3642d8068f702754f965f8d5ce
Chaitannyaa <chaitannyaagaikwad@gmail.com>
2023-04-11 14:57:27 -0500

    Add fundamentals directory

---
+++ fundamentals/Primitive_data_types.py
@@ -0,0 +1,13 @@
+# Primitive and Non-primitive data types
+
+a = 10                  # Interger
+b = 2.4                 # Float
+name = "Chaitannyaa"    # String
+we_are_Humans = True    # Bool
+c = None                # None
+
+print(type(a))
+print(type(b))
+print(type(name))
+print(type(we_are_Humans))
+print(type(c))
---
+++ fundamentals/non-primitive-data-types.py
@@ -0,0 +1,19 @@
+#List
+fruits = [ "Apple", 12, "Mango", 28, "Orange"]
+numbers = [12, 14, "Apple", 15, 19, 12, 24, 15]
+
+#Tuple
+commits = ( "ws21sq", "qwds21", "w2wqa1", "sw1212")
+
+#Set
+set = { 1, 2, 3, 6, 9, 12, 15}
+
+#Dictionary
+mydict = { "Name": "Chaitannyaa", "Mobile_No": 9911334775, 
+          "Languages": ["Hindi", "Marathi", "English"], "Male": True}
+
+print(type(fruits))
+print(type(numbers))
+print(type(commits))
+print(type(set))
+print(type(mydict))
