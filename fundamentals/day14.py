92aa2d3b898e79a4f5a7822d6899c07cf7abdd3d
Chaitannyaa <chaitannyaagaikwad@gmail.com>
2023-04-12 02:26:35 -0400

    Update fundamental data

---
+++ fundamentals/day14.py
@@ -0,0 +1,17 @@
+fav_tools = { 
+  1:"Linux", 
+  2:"Git", 
+  3:"Docker", 
+  4:"Kubernetes", 
+  5:"Terraform", 
+  6:"Ansible", 
+  7:"Chef"
+}
+
+# print(fav_tools[3])
+
+cloud_providers = ["AWS","GCP","Azure"]
+
+cloud_providers.append("Digital Ocean")
+
+print(cloud_providers)
